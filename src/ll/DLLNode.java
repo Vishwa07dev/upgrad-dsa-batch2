1d865ee587093c5f0deceecfc4f8796f2d51d81e
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-05 23:04:35 +0530

    code for class April 5 - part2

---
+++ src/ll/DLLNode.java
@@ -0,0 +1,36 @@
+package ll;
+
+public class DLLNode {
+
+    private int data ;  // 0
+    private DLLNode next ;  // null
+    private DLLNode prev ;  // null
+
+    public DLLNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public DLLNode getNext() {
+        return next;
+    }
+
+    public void setNext(DLLNode next) {
+        this.next = next;
+    }
+
+    public DLLNode getPrev() {
+        return prev;
+    }
+
+    public void setPrev(DLLNode prev) {
+        this.prev = prev;
+    }
+}
---
+++ src/ll/DoublyLinkedList.java
@@ -0,0 +1,99 @@
+package ll;
+
+public class DoublyLinkedList {
+
+
+
+    /**
+     * Traverse a DLL
+     *
+     */
+    public static void traverse(DLLNode head){
+        if(head==null){
+            return ;
+        }
+        System.out.print(head.getData() + " <-> ");
+        traverse(head.getNext());
+    }
+
+    /**
+     * Find the length of the DLL
+     *
+     */
+    public static int length(DLLNode head){
+        return head==null ? 0 : 1 + length(head.getNext());
+    }
+
+    /**
+     * insert an element in DLL
+     *
+     */
+    public static DLLNode insertAtKthPos(DLLNode head, int data, int k){
+
+        if(head ==null && k!=0){   // there is no node
+            return head;
+        }
+        if(k<0 || k>length(head)){
+            return head;
+        }
+        DLLNode newNode = new DLLNode(data);
+        if(k==0){
+            newNode.setNext(head);
+            head.setPrev(newNode);
+            head = newNode;
+        }else{
+            int index =0 ;
+            DLLNode temp = head ;
+            while(index < k-1){
+                temp = temp.getNext();
+                index++;
+            }
+
+            newNode.setNext(temp.getNext());
+            temp.getNext().setPrev(newNode);
+            temp.setNext(newNode);
+            newNode.setPrev(temp);
+        }
+        return head ;
+    }
+
+    public DLLNode insertRec(DLLNode head , int data , int k){
+        if(head ==null && k!=0){   // there is no node
+            return head;
+        }
+        if(k<0 || k>length(head)){
+            return head;
+        }
+        if(k==0){
+            DLLNode newNode = new DLLNode(data);
+            newNode.setNext(head);
+            head.setPrev(newNode);
+            head = newNode;
+        }else{
+            DLLNode tempHead = insertAtKthPos(head.getNext(),data, k-1);
+            tempHead.setPrev(head);
+            head.setNext(tempHead);
+        }
+
+        return  head;
+    }
+
+
+
+    public static void main(String[] args) {
+        DLLNode head = new DLLNode(1);
+        DLLNode node1  = new DLLNode(2);
+        DLLNode node2 = new DLLNode(3);
+        DLLNode node3 = new DLLNode(4);
+        DLLNode node4 = new DLLNode(5);
+
+        head.setNext(node1); node1.setPrev(head);
+        node1.setNext(node2); node2.setPrev(node1);
+        node2.setNext(node3); node3.setPrev(node2);
+        node3.setNext(node4); node4.setPrev(node3);
+
+        traverse(head);
+        System.out.println();
+        System.out.println(length(head));
+    }
+}
