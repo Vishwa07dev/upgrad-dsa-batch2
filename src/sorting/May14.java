65d35ca9c41b00c1f191efe22aa6dc3718cadab1
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-14 23:33:03 +0530

    class codes

--- src/sorting/May14.java
+++ src/sorting/May14.java
@@ -40,12 +40,68 @@ public class May14 {
     }
 
 
-    public static void printUnsortedIdexes(int[] arr){
+    public static void printUnsortedIndexes(int[] arr){
+
+        int start =0, end = arr.length-1;
+
+
+        for(start=0;start<arr.length-1;start++){
+            if(arr[start]>arr[start+1]){
+                break;
+            }
+        }
+        if(start == arr.length-1){
+            System.out.println("Array is already sorted");
+            return;
+        }
+
+        for(end = arr.length-1 ;end>0;end--){
+            if(arr[end]<arr[end-1]){
+                break;
+            }
+        }
+
+        /**
+         * Need to find the max and min value between start and end index
+         */
+        int max = arr[start];
+        int min = arr[start];
+
+        for(int i=start;i<=end;i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+
+        /**
+         * Check in the left part of the array
+         */
+        for(int i=0;i<start;i++){
+            if(arr[i]>min){
+                start=i;
+                break;
+            }
+        }
+
+        for(int i=arr.length-1;i>end;i--){
+            if(arr[i]<max){
+                end = i;
+            }
+        }
+
+        System.out.println("Array should be sorted between indexes "+ start + " and "+end );
 
     }
 
 
     public static void main(String[] args) {
+
+        int[] arr = {10,12,20,30,25,40,32,31,35,50,60};
+
+        printUnsortedIndexes(arr);
 
     }
 
