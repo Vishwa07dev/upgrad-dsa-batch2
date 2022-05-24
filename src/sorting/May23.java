9710b11a3a747a38f3004a431a3ba9ecccd0441a
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-24 22:27:25 +0530

    added the code

--- src/sorting/May23.java
+++ src/sorting/May23.java
@@ -5,6 +5,39 @@ import java.util.Arrays;
 public class May23 {
 
 
+    /**
+     * kthSmallestElement(arr, 1, 7, 4) -> return the 4th smallest element present between
+     * index 1 and 7 in the given array
+     *
+     */
+
+    public static int kthSmallestElement(int[] arr, int left , int right, int k){
+
+
+        if(k>0 && k<=right-left+1){  // right-left+1  = size of the array
+            /**
+             * Write the logic to find the kth element
+             */
+            int pi = pivotIndex(arr, left,right);
+
+            if(pi-left+1 ==k){
+                //pi index is the index of kth smallest element
+                return arr[pi];
+            }else if( pi-left+1 >k){
+                return kthSmallestElement(arr, left,pi-1,k);
+            }else{
+                return kthSmallestElement(arr, pi+1,right,k);
+            }
+
+        }else{
+            return Integer.MAX_VALUE;
+        }
+    }
+
+
+
+
+
     public static int pivotIndex(int[] arr , int start, int end){
 
         int pivot = arr[end]; // last element has been choosen as the pivot
@@ -38,12 +71,16 @@ public class May23 {
 
         int pi = pivotIndex(arr,start, end);
 
-        /**
-         * everything to the left of pi will be smaller
-         * and everything to the right of pi will be bigger
-         */
         quickSort(arr,start,pi-1);
         quickSort(arr,pi+1,end);
+    }
+
+
+    public static void permutations(int n){
+
+        System.out.println(n);
+
+
     }
 
     private static void swap(int[] arr, int p, int i) {
