dc3b9ef35ad856ca299cc7909b6fd506f98a79dd
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-05-30 23:22:43 +0530

    added the code

--- src/searching/May302022.java
+++ src/searching/May302022.java
@@ -32,4 +32,51 @@ public class May302022 {
         return noOfWaysUtils(target-num, num+1) + // Choose the num
                 noOfWaysUtils(target, num+1); // ignore the num
     }
+
+
+    public int findEquilibriumIndex(int[] arr){
+       int left  =0 ;
+       int right = arr.length-1;
+       int mid = left + (right-left)/2;
+
+        /**
+         * Find the left sum  0 to mid-1 index
+         */
+        int sumLeft = arr[0];
+        for(int i=1;i<mid;i++){
+            sumLeft = sumLeft+arr[i];
+        }
+
+        /**
+         * Find the right sum mid+1 to last index
+         */
+        int sumRight = arr[mid+1];
+        for(int i= mid+2;i<arr.length;i++){
+            sumRight = sumRight + arr[i];
+        }
+
+        if(sumRight > sumLeft){
+            /**
+             * We need to take few elements from right and add few elements in left
+             */
+            while(sumRight>sumLeft && mid<arr.length-1){
+                sumLeft = sumLeft+ arr[mid];
+                sumRight = sumRight - arr[mid+1];
+                mid = mid+1;
+            }
+        }else if(sumRight<sumLeft){
+            /**
+             * We need to take few elements from left and add to the elements in right
+             */
+            while(sumLeft>sumRight && mid>0){
+                sumRight = sumRight +arr[mid];
+                sumLeft = sumLeft - arr[mid-1];
+                mid = mid-1;
+            }
+        }
+        if(sumLeft == sumRight){
+            return mid;
+        }
+        return -1;
+    }
 }
