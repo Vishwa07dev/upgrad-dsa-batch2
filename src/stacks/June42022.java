6fbeda2cbf81b24e922bc6216eb3d406647afb7b
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-06-04 22:59:20 +0530

    added code for stack using array

---
+++ src/stacks/June42022.java
@@ -0,0 +1,67 @@
+package stacks;
+
+public class June42022 {
+}
+
+class StackUsingArray {
+
+    private int[] arr ;  // this will hold data
+    private int capacity ; // total capacity of the Stack
+    private int top; // this will represent the index of last element added
+
+    public StackUsingArray(int capacity) {
+        this.capacity = capacity;
+        this.arr = new int[capacity];
+        top = -1 ;// initially there is no element
+
+    }
+
+    /**
+     * PUSH
+     */
+    public void push(int data) {
+        /**
+         * If it's already full
+         */
+        if(isFull()){
+            throw new RuntimeException("Stack overflow exception");
+        }
+        this.arr[++top] = data;
+    }
+
+    /**
+     * POP
+     */
+    public int pop(){
+        if(isEmpty()){
+            throw new RuntimeException("Stack underflow exception");
+        }
+        return this.arr[this.top--];
+    }
+
+    /**
+     * isEmpty
+     */
+    public boolean isEmpty(){
+        return this.top == -1  ;
+    }
+
+    /**
+     * isFull
+     */
+    public boolean isFull(){
+      return this.top == this.capacity-1;
+    }
+
+    /**
+     * top
+     */
+    public int top(){
+        if(isEmpty()){
+            throw new RuntimeException("Stack underflow exception");
+        }
+        return arr[this.top]; // just returning the value
+    }
+
+
+}
