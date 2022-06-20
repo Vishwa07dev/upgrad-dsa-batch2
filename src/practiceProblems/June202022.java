9b7211e97b9c0a27074071921c1e1f65d2956f5b
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-06-20 22:33:51 +0530

    class code

---
+++ src/practiceProblems/June202022.java
@@ -0,0 +1,25 @@
+package practiceProblems;
+
+import ll.ListNode;
+
+public class June202022 {
+
+    public static void printAlternate(ListNode head){
+
+        if(head ==null){
+            return;
+        }
+
+        while(head!=null){
+            System.out.print(head.getData());
+
+            head = head.getNext();
+            if(head==null){
+                return;
+            }
+            head= head.getNext();
+        }
+
+    }
+
+}
