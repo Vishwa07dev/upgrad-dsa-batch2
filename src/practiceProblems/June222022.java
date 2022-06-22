0e7ec8bd06cb7fae5474b8f8f7560060005e47e6
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-06-22 23:19:11 +0530

    class code

--- src/practiceProblems/June222022.java
+++ src/practiceProblems/June222022.java
@@ -1,9 +1,30 @@
 package practiceProblems;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class June222022 {
+
+
+    public static void findSymmPair(int[][] arr){
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+            int first = arr[i][0];
+            int second = arr[i][1];
+            /**
+             * I need to go and check second in the map
+             */
+            if(map.get(second)!=null && map.get(second)==first){
+                System.out.println("( "+first + " , "+ second +" )");
+            }else{
+                map.put(first,second);
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -16,9 +37,6 @@ public class June222022 {
 
     private static void sort(int[] arr) {
 
-        /**
-         * 1. Find the largest element in the array
-         */
         int largest = arr[0];
 
         for(int i=1;i<arr.length;i++){
@@ -26,35 +44,18 @@ public class June222022 {
                 largest = arr[i];
             }
         }
-
-        /**
-         * 2. We need to create a new array of size largest+1
-         */
         int[] count = new int[largest+1];
 
-        /**
-         * 3. Iterate the arr, and put all the elements into count array
-         */
         for(int i=0;i<arr.length;i++){
             //count[arr[i]] = count[arr[i]]+1;
             ++count[arr[i]];
         }
 
-        /**
-         * 4. We try to find the right position of each element
-         */
-        for(int i=1;i<count.length;i++){
+         for(int i=1;i<count.length;i++){
             //count[i] = count[i]+count[i-1];
             count[i] +=count[i-1];
         }
-        /**
-         * count[i] would represent the correction position of i for
-         * the sorted arrangement
-         */
 
-        /**
-         * 5. Using the count array, we need to sort the array
-         */
         int[] temp = new int[arr.length]; // same size as the original array
 
         for(int i= arr.length-1;i>=0;i--){
@@ -63,14 +64,50 @@ public class June222022 {
             --count[arr[i]];
         }
 
-        /**
-         * 6.I need to sort the original array
-         * Copy the elements from temp to the array
-         */
         for(int i=0;i<arr.length;i++){
             arr[i]=temp[i];
         }
 
 
     }
+}
+
+
+class Stacks {
+    private int[] arr ;
+    private int capacity;
+    int top1,top2 ; // top1 will be used for stack1 and top2 will be used for stack2
+
+    public Stacks(int capacity) {
+        this.capacity = capacity;
+        this.arr = new int[this.capacity];
+
+        int mid = capacity/2;
+        top1 = mid;
+        top2=mid+1;
+    }
+
+    public void push1(int data){
+        if(isFullStack1()){
+            throw new RuntimeException("Stack overflow");
+        }
+        this.arr[top1--] = data;
+    }
+
+    public int pop1(){
+        if(isEmptyStack1()){
+            throw new RuntimeException("Stack underflow");
+        }
+        int data = this.arr[++top1];
+        return data ;
+    }
+
+    private boolean isEmptyStack1() {
+        return top1==this.capacity/2;
+    }
+
+    public boolean isFullStack1(){
+       return top1==0;
+    }
+
 }
